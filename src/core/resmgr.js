21484daffbf20ee4746ceaa8e6639d7b146cd2b5
Darryl Pogue <dvpdiner2@gmail.com>
2012-04-29 22:17:45 -0700

    Used shared variables for ResMgr.
    You shouldn't ever have more than one Mgr, but it would be nice to be
    able to make a bunch of them that all share the same internal state.
    I also think it makes the code a bit more organized.

--- src/core/resmgr.js
+++ src/core/resmgr.js
@@ -24,135 +24,135 @@ if (typeof define !== 'function') {
 }
 
 define(['./key'], function(Key) {
+    "use strict";
 
-    /**
-     * Singleton that manages all the objects and object types.
-     * @variable factory An array of type constructors.
-     * @variable keys An array of arrays of objects, keyed by type and index.
-     */
-    var ResManager = function() {
-        var objdef = function() {
-            this.factory = [];
-            this.keylist = [];
-        };
+    // Semi-Global Static Variables
 
-        objdef.prototype.register_class = function(klass) {
-            if (!(klass instanceof Function)) {
-                throw 'Trying to register a non-class type';
+    /** An array of type constructors. */
+    var _factory = [];
+
+    /** An array of arrays of objects, keyed by type and index. */
+    var _keylist = [];
+
+
+    // The ResManager Object
+    /** Manager to keep track of all objects, states, and types. */
+    function ResManager() { }
+
+    ResManager.prototype.register_class = function(klass) {
+        if (!(klass instanceof Function)) {
+            throw 'Trying to register a non-class type';
+        }
+        var type = klass.prototype.ClsIdx;
+
+        if (_factory[type]) {
+            console.warn('Registering with an index that is already used!');
+        }
+
+        _factory[type] = klass;
+    };
+
+    ResManager.prototype.new_object = function(type) {
+        if (!_factory[type]) {
+            throw 'Trying to create an object of an nonexistent type!';
+        }
+        var obj = new _factory[type]();
+        var key = new Key();
+        var idx = 0;
+        if (_keylist[type]) {
+            idx = _keylist[type].length;
+        }
+        key.read({'type': type, 'index': idx});
+        obj.key = key;
+        this.register_key(key, obj);
+        return obj;
+    };
+
+    ResManager.prototype.add_object = function(key) {
+        if (this.find(key)) {
+            return;
+        }
+
+        var obj = new _factory[key.type]();
+        obj.key = key;
+        this.register_key(key, obj);
+        return obj;
+    };
+
+    ResManager.prototype.register_key = function(key, obj) {
+        if (!(key instanceof Key)) {
+            throw 'Invalid key passed to ResManager.register_key!';
+        }
+
+        if (!_keylist[key.type]) {
+            _keylist[key.type] = [];
+        }
+        _keylist[key.type][key.index] = obj;
+    };
+
+    ResManager.prototype.find = function(key) {
+        if (!(key instanceof Key)) {
+            throw 'Invalid key passed to ResManager.find!';
+        }
+
+        if (!_keylist[key.type]) {
+            return null;
+        }
+
+        var obj = _keylist[key.type][key.index];
+        if (!obj) {
+            return null;
+        }
+        return obj;
+    };
+
+    ResManager.prototype.find_or_create = function(key) {
+        var obj = this.find(key);
+
+        if (!obj) {
+            obj = this.add_object(key);
+        }
+
+        return obj;
+    };
+
+    ResManager.prototype.read = function(s) {
+        if (!s.key) {
+            throw "Tried to read an object without a key!";
+        }
+
+        var key = new Key();
+        key.read(s.key);
+
+        var obj = this.find_or_create(key);
+        obj.read(s);
+        obj.on_loaded();
+
+        return obj;
+    };
+
+    ResManager.prototype.load = function(source) {
+        if (source instanceof Array) {
+            /* We got an array of objects */
+            source.forEach(function(o) {
+                this.read(o);
+            }, this);
+        } else if (source instanceof String) {
+            /* We have a JSON string */
+            var data = JSON.parse(source);
+            if (!(data instanceof Array)) {
+                throw 'Invalid JSON data';
             }
-            type = klass.prototype.ClsIdx;
-
-            if (this.factory[type]) {
-                console.warn('Registering with an index that is already used!');
-            }
-
-            this.factory[type] = klass;
-        };
-
-        objdef.prototype.new_object = function(type) {
-            if (!this.factory[type]) {
-                throw 'Trying to create an object of an nonexistent type!';
-            }
-            var obj = new this.factory[type]();
-            var key = new Key();
-            var idx = 0;
-            if (this.keylist[type]) {
-                idx = this.keylist[type].length;
-            }
-            key.read({'type': type, 'index': idx});
-            obj.key = key;
-            this.register_key(key, obj);
-            return obj;
-        };
-
-        objdef.prototype.add_object = function(key) {
-            if (this.find(key)) {
-                return;
-            }
-
-            var obj = new this.factory[key.type]();
-            obj.key = key;
-            this.register_key(key, obj);
-            return obj;
-        };
-
-        objdef.prototype.register_key = function(key, obj) {
-            if (!(key instanceof Key)) {
-                throw 'Invalid key passed to ResManager.register_key!';
-            }
-
-            if (!this.keylist[key.type]) {
-                this.keylist[key.type] = [];
-            }
-            this.keylist[key.type][key.index] = obj;
-        };
-
-        objdef.prototype.find = function(key) {
-            if (!(key instanceof Key)) {
-                throw 'Invalid key passed to ResManager.find!';
-            }
-
-            if (!this.keylist[key.type]) {
-                return null;
-            }
-
-            var obj = this.keylist[key.type][key.index];
-            if (!obj) {
-                return null;
-            }
-            return obj;
-        };
-
-        objdef.prototype.find_or_create = function(key) {
-            var obj = this.find(key);
-
-            if (!obj) {
-                obj = this.add_object(key);
-            }
-
-            return obj;
-        };
-
-        objdef.prototype.read = function(data) {
-            if (!data.key) {
-                throw "Tried to read an object without a key!";
-            }
-
-            var key = new Key();
-            key.read(data.key);
-
-            var obj = this.find_or_create(key);
-            obj.read(data);
-            obj.on_loaded();
-
-            return obj;
-        };
-
-        objdef.prototype.load = function(source) {
-            if (source instanceof Array) {
-                /* We got an array of objects */
-                source.forEach(function(o) {
-                    this.read(o);
-                }, this);
-            } else if (source instanceof String) {
-                /* We have a JSON string */
-                var data = JSON.parse(source);
-                if (!(data instanceof Array)) {
-                    throw 'Invalid JSON data';
-                }
-                data.forEach(function(o) {
-                    this.read(o);
-                }, this);
-            } else {
-                console.error('Unknown source data passed to ResManager!');
-                console.warn('Silently continuing');
-            }
-        };
-
-        return objdef;
-    }();
+            data.forEach(function(o) {
+                this.read(o);
+            }, this);
+        } else {
+            console.error('Unknown source data passed to ResManager!');
+            console.warn('Silently continuing');
+        }
+    };
 
     var globalResMgr = new ResManager(); /* Global Resource Manager */
 
-    return (function() { return globalResMgr; }());
+    return (function() { return globalResMgr; })();
 });
