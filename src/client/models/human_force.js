646e1d149588a3d40a2016ce540943564b05b9dd
Tom Nightingale <tom@tnightingale.com>
2012-05-23 22:33:44 -0700

    Human input now maintains internal state.
    Instead of calculating on property request and breaking inheritance model.

--- src/client/models/human_force.js
+++ src/client/models/human_force.js
@@ -9,15 +9,27 @@ define([
         up: '38', down: '40'
     };
 
-    var Input = function() {
+    /**
+     * Human input handler.
+     * Transforms keyboard input into directional force.
+     * Implements vector interface.
+     */
+    var obj = function() {
         var _input = '_human_input_input',
             _keys = '_human_input_keys',
             _handler = '_human_input_handler';
 
-        var objdef = function(keys) {
+        /**
+         * Input constructor.
+         *
+         * @param keys
+         *      An object containing keycodes mapped to each directional key
+         *      alias; LEFT, RIGHT, UP and DOWN.
+         */
+        var HumanInput = function(keys) {
             this[_keys] = keys || default_keys;
             this[_input] = {};
-            this[_handler] = input_handler(this[_input]);
+            this[_handler] = input_handler.bind(this);
 
             // Init input state.
             for (var direction in this[_keys]) {
@@ -29,49 +41,45 @@ define([
             doc.addEventListener('keyup', this[_handler]);
         };
 
-        /** Horizontal direction. */
-        Object.defineProperty(objdef.prototype, 'x', {
-            get: function () { 
-                var left = this[_keys].left,
-                    right = this[_keys].right;
+        // Keeping things DRY.
+        HumanInput.prototype.direction_x = direction(['left', 'right']);
+        HumanInput.prototype.direction_y = direction(['up', 'down']);
 
-                if (this[_input][left] ^ this[_input][right]) {
-                    return this[_input][left] ? -1 : 1;
+        /**
+         * Returns function to calculate whether a value should be +'ve or -'ve
+         * depending on which keys have been pushed.
+         *
+         * @param tuple
+         *      An array containing two key aliases; First for -'ve direction,
+         *      2nd for +'ve direction
+         */
+        function direction (tuple) {
+            return function () {
+                var neg = this[_keys][tuple[0]],
+                    pos = this[_keys][tuple[1]];
+
+                if (this[_input][neg] ^ this[_input][pos]) {
+                    return this[_input][neg] ? -1 : 1;
                 }
                 return 0;
-            }
-        });
+            };
+        }
 
-        /** Vertical direction. */
-        Object.defineProperty(objdef.prototype, 'y', {
-            get: function () { 
-                var up = this[_keys].up,
-                    down = this[_keys].down;
-
-                if (this[_input][up] ^ this[_input][down]) {
-                    return this[_input][up] ? -1 : 1;
-                }
-                return 0;
-            }
-        });
-
-        return objdef;
-    }();
-
-    // Handling input.
-    function input_handler(state) {
-        var input = state;
-        return function (evt) {
+        function input_handler (evt) {
             var event = (evt.type === 'keydown') ? 1 : 0,
                 key = evt.keyCode;
 
-            if (input.hasOwnProperty(key) && event !== input[key]) {
-                input[key] = event;
+            if (this[_input].hasOwnProperty(key) && event !== this[_input][key]) {
+                this[_input][key] = event;
+                this.x = this.direction_x();
+                this.y = this.direction_y();
             }
-        };
-    }
+        }
 
-    Util.inherits(Input, Vector);
+        return HumanInput;
+    }();
 
-    return Input;
+    Util.inherits(obj, Vector);
+
+    return obj;
 });
