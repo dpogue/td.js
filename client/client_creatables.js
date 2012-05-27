aae014db4c8ed20b21677031fd4a9137ead99117
Faris Chebib <faris@theluckybead.com>
2012-05-27 14:23:23 -0700

    added the map img and model ref

--- client/client_creatables.js
+++ client/client_creatables.js
@@ -1,10 +1,12 @@
 define([
     'src/client/models/unit',
+    'src/client/models/map',
     'src/models/player',
     'src/client/models/tower',
     'src/models/npc'
 ], function(
     Unit,
+		Map,
     Player,
     Tower,
     NPC
